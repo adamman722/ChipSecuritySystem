1825edd79f2be8bbfca3cbe631f60cfefca80775
adamman722 <tarpeyadam@gmail.com>
2023-03-12 00:44:18 -0500

    Removed one comment

--- Program.cs
+++ Program.cs
@@ -48,54 +48,6 @@ namespace ChipSecuritySystem
                 Console.WriteLine(data.Item1);
                 Console.ReadLine();
             }
-
-
-            //FindBlue fb = new FindBlue(colorChips);
-
-            //List<ColorChip> colorChipsSorted = fb.FindBlueInList();
-
-            //if (colorChipsSorted[0] == null)
-            //{
-            //    Console.WriteLine("Cannot be done there is no blue starting chip");
-            //    Console.ReadLine();
-            //}
-            //else
-            //{
-            //FindGreen fg = new FindGreen(colorChipsSorted);
-            //colorChipsSorted = fg.FindGreenInList();
-
-            ////endtry point for function
-            // if (colorChipsSorted.Last() == null)
-            //{
-            //    Console.WriteLine("Cannot be done there is no green ending chip");
-            //    Console.ReadLine();
-            //}
-            //else
-            //{
-            //    FindLinks fl = new FindLinks();
-            //    fl.sortFunction2(colorChipsSorted);
-            //    List<ColorChip> sorted = fl.ReturnSorted();
-
-            //    if (sorted == null)
-            //    {
-            //        Console.WriteLine("Could not find matching links");
-            //        Console.ReadLine();
-            //    }
-            //    else
-            //    {
-
-            //    foreach (var item in sorted)
-            //    {
-            //        Console.WriteLine(item);
-            //    }
-            //    Console.WriteLine(true);
-            //    Console.ReadLine();
-            //    }
-
-            //}
-
-            //}
-
         }
     }
 }
